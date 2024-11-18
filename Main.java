967f1957604a139b3b58c039bec86db6236af077
siddshah1234 <151389521+siddshah1234@users.noreply.github.com>
2024-11-18 23:51:36 +0000

    comitt :)

---
+++ Main.java
@@ -0,0 +1,67 @@
+// Siddhant Shah
+// Computer Science Programming II
+// 11/19/2024
+// Project Name: Employee Salary Calculator 
+
+// importing packages
+import java.util.Scanner;
+import java.lang.Math;
+public class Main
+{ 
+	public static void main(String [] args)
+	{
+		Scanner scanner = new Scanner(System.in);
+		// taking in number of payments per month
+		System.out.println("Hello! Please enter the number of payments the employee recieves per month: "); // <-- i/o functionality: printing out 
+		int monthlypayments = scanner.nextInt(); // <-- example of data type: int
+        //                  ^ example of i/o functionality: taking input from the user using scanner.nextInt()
+        
+        // taking in amount per paycheck 
+		System.out.println("Enter the amount of money the employee recieves per payment: ");
+		double paycheckamount = scanner.nextDouble(); // <-- example of data type: double
+
+        // taking in percentage of salary that is taxed
+		System.out.println("Enter the percentage of the employee's salary that is taxed: ");
+		double taxamount = scanner.nextDouble()/100;
+
+        // taking in average raise amount annually
+		System.out.println("What is the employee's annual raise amount as a percentage (on average): ");
+		double raiseamount = scanner.nextDouble()/100;
+		raiseamount+=1; // <-- example of arithmetic operation manipulating data
+
+        // getting variables for finding final salary and calculating to find current yearly pay before taxes
+		double calcsalary;
+		double yearly = paycheckamount*monthlypayments*12;
+	
+        // input to get salary after x amount of years (user inputs x)
+		System.out.println("Enter your salary after x amount of years. Enter x (or no to stop): ");
+		String years = scanner.next(); // <-- example of data type: string
+		
+	    // performing calculations to find user salary after x amount of years after taxes
+	    // converting string into double
+		calcsalary = Double.parseDouble(years); // <-- data manipulation technique using parseDouble function
+		// finding how much to multiply the current salary by
+		calcsalary = Math.pow(raiseamount, calcsalary); 
+		// multiplying the current salary to get the future salary
+		calcsalary = yearly*calcsalary;
+		// taking away salary for taxes
+		calcsalary-=(calcsalary*taxamount);
+		// printing out monthly and yearly projected salary of the user
+		System.out.print("\nYour salary in " + years + " years will be $" + Math.round(calcsalary/12) + " per month.");
+		System.out.print("\nYour salary in " + years + " years will be $" + Math.round(calcsalary) + " per year.");
+
+		
+
+	
+
+	}
+
+
+
+}
+
+
+
+
+
+
